ad12a9b2f46c86ec26274874f049df84d92c423d
Zeide <zeide.thibaud@gmail.com>
2021-10-05 17:03:46 +0200

    Upgraded lot of dependencies and gradle 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.10"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 
-    id("com.github.ben-manes.versions") version "0.39.0"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "com.zeide"
-version = "1.0-ALPHA"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -18,23 +17,23 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
 
-    implementation("io.ktor:ktor-client-core:1.6.0")
-    implementation("io.ktor:ktor-client-serialization:1.6.0")
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("io.ktor:ktor-client-serialization:1.6.4")
 
-    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
-    implementation("io.github.microutils:kotlin-logging:2.0.8")
+    implementation("io.github.microutils:kotlin-logging:2.0.11")
 
-    implementation("dev.kord:kord-core:0.7.4")
+    implementation("dev.kord:kord-core:0.8.0-M5")
     implementation("dev.kord.x:emoji:0.5.0")
 
     implementation("de.androidpit:color-thief:1.1.2")
 
-    implementation("org.apache.tika:tika-core:1.26")
+    implementation("org.apache.tika:tika-core:2.0.0")
 }
 
 tasks.withType<Jar> {
